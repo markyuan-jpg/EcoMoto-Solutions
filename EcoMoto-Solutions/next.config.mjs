dde4bfc8d1096c09c265891bc538c590559cb01e
markyuan <markyuanbo@gmail.com>
2026-04-19 23:22:36 +0800

    Debug tailwind config

--- EcoMoto-Solutions/next.config.mjs
+++ EcoMoto-Solutions/next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    // Force Tailwind processing
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'postcss-loader',
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
